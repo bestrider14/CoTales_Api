0a02eecb3442d935409554993f931f54068c6248
Luka <luka.dupuis.1@ulaval.ca>
2025-12-29 00:12:40 -0500

    Init

---
+++ src/main/java/com/cotales/cotales_api/CoTalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.cotales.cotales_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoTalesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoTalesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cotales/cotales_api/CoTalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotales.cotales_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoTalesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
