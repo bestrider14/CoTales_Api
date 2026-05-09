08b0c01612bcdbdf88dcb32d46a0d5d822d36fdf
Luka <luka.dupuis.1@ulaval.ca>
2026-05-09 15:56:59 -0400

    chore: start fresh new

--- src/main/java/com/cotales/cotales_api/CoTalesApiApplication.java
+++ src/main/java/com/cotales/cotales_api/CoTalesApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoTalesApiApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(CoTalesApiApplication.class, args);
     }
 
